56c2d2ebaaa64f501e0ebe108d69f60a72dc15a0
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-11-26 16:42:00 +0530

    feat : added logger to service

--- src/modules/attendance/attendance.module.ts
+++ src/modules/attendance/attendance.module.ts
@@ -4,12 +4,13 @@ import { AttendanceController } from './attendance.controller';
 import { AttendanceEntity } from './entities/attendance.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtStrategy } from "src/common/guards/keycloak.strategy";
+import { LoggerService } from 'src/common/logger/logger.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AttendanceEntity]),
   ],
   controllers: [AttendanceController],
-  providers: [AttendanceService,JwtStrategy]
+  providers: [AttendanceService, JwtStrategy, LoggerService]
 })
-export class AttendanceModule {}
+export class AttendanceModule { }
